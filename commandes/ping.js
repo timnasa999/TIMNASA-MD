40959dfd596d1a10df9402d816067bf14c5d2f4a
FELIXTZ <hanstzgamestech@gmail.com>
2024-11-26 14:27:34 +0300

    Add files via upload

---
+++ commandes/ping.js
@@ -0,0 +1,24 @@
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { default: axios } = require('axios');
+//const conf = require('../set');
+
+
+zokou({ nomCom: 'ping',
+    desc: 'To check ping',
+    Categorie: 'General',
+    reaction: '🚀', 
+    fromMe: 'true', 
+
+       
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+    const { start} = new Date().getTime()
+    return repondre('*TIMNASA-MD-SPEED-IS*\n ```' +99999999999+ '``` *ᴍ/s*') 
+    const { end } = new Date().getTime()
+    await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
+  }
+)
+
+
---
+++ commandes/repos.js
@@ -0,0 +1,46 @@
+﻿"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+
+zokou({ nomCom: "repo", catégorie:"General", reaction: "👊", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+  const githubRepo = 'https://api.github.com/repos/Halisi1234/TIMNASA-MD';
+  const img = 'https://files.catbox.moe/3vkm13.jpeg';
+
+  try {
+    const response = await fetch(githubRepo);
+    const data = await response.json();
+
+    if (data) {
+      const repoInfo = {
+        stars: data.stargazers_count,
+        forks: data.forks_count,
+        lastUpdate: data.updated_at,
+        owner: data.owner.login,
+      };
+
+      const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
+      const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
+
+      const gitdata = `*ʜᴇʟʟᴏᴡ ᴡʜᴀᴛsᴀᴀᴘ ᴜsᴇʀ
+ᴛʜɪs ɪs* *TIMNASA-MD .*\n sᴜᴘᴘᴏʀᴛ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ *ʙʏ*,  https://chat.whatsapp.com/BM7F8CC4yMO9iJynKkiflU
+
+╔═━━━━════──────➳
+║╔═━━━━━━════─━━─➳
+║║ 🗼 *REPOSITORY:* ${data.html_url}
+║║ 🌟 *STARS:* ${repoInfo.stars}
+║║ 🧧 *FORKS:* ${repoInfo.forks}
+║║ 📅 *RELEASE DATE:* ${releaseDate}
+║║ 🕐 *UPDATE ON:* ${repoInfo.lastUpdate}
+║║ 👨‍💻 *OWNER:* *HANSTZTECH*
+║║ 💞 *NAME:* *HANS-MD *
+║║ 🥰 *ENJOY TO USE TIMNASA MD * ╚══━━━━════─━━━━──➳
+╚══━━━━════─━━━━──➳`;
+
+      await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
+    } else {
+      console.log("Could not fetch data");
+    }
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+});
